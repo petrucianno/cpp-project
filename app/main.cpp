647ffc15ef1ec00673f58c60b63fc0abf2874be6
Petru Gurduza <7842656+petrucianno@users.noreply.github.com>
2022-10-17 17:54:55 +0300

    Remove unneded boilerplate
    Signed-off-by: Petru Gurduza <7842656+petrucianno@users.noreply.github.com>

--- app/main.cpp
+++ app/main.cpp
@@ -1,23 +1,11 @@
-// Executables must have the following defined if the library contains
-// doctest definitions. For builds with this disabled, e.g. code shipped to
-// users, this can be left out.
-#ifdef ENABLE_DOCTEST_IN_LIBRARY
-#define DOCTEST_CONFIG_IMPLEMENT
-#include "doctest/doctest.h"
-#endif
 
 #include <iostream>
-#include <stdlib.h>
+#include <cstdlib>
 
 #include "exampleConfig.h"
-#include "example.h"
 
-/*
- * Simple main program that demontrates how access
- * CMake definitions (here the version number) from source code.
- */
 int main() {
-  std::cout << "C++ Boiler Plate v"
+    std::cout << "Hello, Boilerplate C++ v"
             << PROJECT_VERSION_MAJOR
             << "."
             << PROJECT_VERSION_MINOR
@@ -26,10 +14,5 @@ int main() {
             << "."
             << PROJECT_VERSION_TWEAK
             << std::endl;
-  std::system("cat ../LICENSE");
-
-  // Bring in the dummy class from the example source,
-  // just to show that it is accessible from main.cpp.
-  Dummy d = Dummy();
-  return d.doSomething() ? 0 : -1;
+    return 0;
 }
--- src/example.cpp
+++ src/example.cpp
@@ -5,17 +5,6 @@ Dummy::Dummy() {
 }
 
 bool Dummy::doSomething() {
-    // Do silly things, using some C++17 features to enforce C++17 builds only.
-    constexpr int digits[2] = {0, 1};
-    auto [zero, one] = digits;
-    return zero + one;
+    return true;
 }
 
-
-#ifdef ENABLE_DOCTEST_IN_LIBRARY
-#include "doctest/doctest.h"
-TEST_CASE("we can have tests written here, to test impl. details")
-{
-    CHECK(true);
-}
-#endif
